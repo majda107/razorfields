c985b2f1dd3691a676fe750ff75853e25b79dadb
majda107 <majda.trpkos@gmail.com>
2021-07-05 23:17:59 +0200

    razor fields base setup

---
+++ RazorFields/Extension/IServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorFields.Extension
+{
+    public static class IServiceCollectionExtension
+    {
+        public static void AddRazorFields(this IServiceCollection services)
+        {
+        }
+    }
+}
